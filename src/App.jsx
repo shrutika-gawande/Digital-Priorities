c87abc519b972fb7b52e9268f789b598f6a93748
shrutika-gawande <shrutikagawande80@gmail.com>
2025-12-05 11:39:47 +0530

    Fixed Cta import issue and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Services from './components/Services';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import About from './components/About';
-import Cta from './components/cta';
+import Cta from './components/Cta';
 import ProjectsPage from './components/ProjectsPage';
 import Team from './components/Team';
 
